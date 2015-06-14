833296333353ea27626e140a3d2b26c30a5ad509
Sandeep Saldanha <supersan83@gmail.com>
2015-06-14 00:09:37 -0400

    Question Creator now has a button

--- WhatDo/app/src/main/java/com/whatdo/whatdoco/whatdo/SplashActivity.java
+++ WhatDo/app/src/main/java/com/whatdo/whatdoco/whatdo/SplashActivity.java
@@ -1,5 +1,7 @@
 package com.whatdo.whatdoco.whatdo;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,6 +13,14 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(mainIntent);
+            }
+        }, 5000);
     }
 
     @Override
